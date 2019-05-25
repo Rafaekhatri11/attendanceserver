d558c1b56eafe0491e48444877af4651b6bca2d3
Rafaekhatri11 <Rafaekhatri11@gmail.com>
2019-05-26 03:15:14 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+
+
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
+
+
+app.get('/', (req,res)=>{
+    res.send("Hello World");
+})
+const port = process.env.port || 5000;
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})

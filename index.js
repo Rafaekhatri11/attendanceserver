d7950b39a202f09cf9cf8232d9e2e8d2eeb1fe16
Rafaekhatri11 <Rafaekhatri11@gmail.com>
2019-05-27 03:08:14 +0500

    email code working

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+var nodemailer = require('nodemailer')
+
 
 
 app.use(bodyParser.urlencoded({extended:true}))
@@ -10,6 +13,60 @@ app.use(cors());
 
 app.get('/', (req,res)=>{
     res.send("Hello World");
+})
+
+
+app.post('/emailapi', (req,res) =>{
+    console.log(req.body)
+    var transporter = nodemailer.createTransport({
+        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth:{
+            user: "safetoschoolsfb@gmail.com",
+            pass : "Sts$2020"
+        }
+    });
+   
+    
+        let data = req.body;
+        data.map((text,index) => {
+            console.log (text)
+            if(text.absent === true){
+                console.log("absent");
+
+                let mailOptions = {
+                    from: 'safetoschoolsfb@gmail.com', // sender address
+                    to: text.email, // list of receivers
+                    subject: "Student Alert", // Subject line
+                    text: "This is the eamil from safetoschools", // plain text body
+                    html: `<b>This is inform you that ${text.name} is absent today </b>` // html body
+                }
+                let info = transporter.sendMail(mailOptions).then( (err, res) => {
+                    console.log(res, "iuoeiwruoiw");
+                }).catch((errr) => {
+                    console.log(errr);
+                })
+            }
+            else if(text.late === true){
+                console.log("late");
+                let mailOptions = {
+                    from: 'safetoschoolsfb@gmail.com', // sender address
+                    to: text.email, // list of receivers
+                    subject: "Student Alert", // Subject line
+                    text: "This is the eamil from safetoschools", // plain text body
+                    html: `<b>This is inform you that ${text.name} will late today </b>` // html body
+                }
+                let info = transporter.sendMail(mailOptions).then( (err, res) => {
+                    console.log(res, "iuoeiwruoiw");
+                }).catch((errr) => {
+                    console.log(errr);
+                })
+            
+            }
+        })
+        
 })
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
